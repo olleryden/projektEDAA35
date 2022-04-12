eb226c073b0422a90e317963279f50799de6e0a8
Olle Rydén <90613060+olleryden@users.noreply.github.com>
2022-04-12 15:25:21 +0200

    BucketSorter Done?

---
+++ BucketSorter.java
@@ -0,0 +1,44 @@
+package sorters;
+
+import java.util.*;
+import java.util.Collections;
+
+public class BucketSorter {
+
+	// Function to sort arr[] of size n
+	// using bucket sort
+	public void sort(ArrayList<Integer> arr) {
+		int n = arr.size();
+		if (n <= 0)
+			return;
+
+		// 1) Create n empty buckets
+		@SuppressWarnings("unchecked")
+		Vector<Integer>[] buckets = new Vector[n];
+
+		for (int i = 0; i < n; i++) {
+			buckets[i] = new Vector<Integer>();
+		}
+
+		// 2) Put array elements in different buckets
+		for (int i = 0; i < n; i++) {
+			Integer idx = arr.get(i);
+			buckets[idx].add(arr.get(i));
+		}
+
+		// 3) Sort individual buckets
+		for (int i = 0; i < n; i++) {
+			Collections.sort(buckets[i]);
+		}
+
+		// 4) Concatenate all buckets into arr[]
+		int index = 0;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < buckets[i].size(); j++) {
+				arr.set(index,  buckets[i].get(j));
+				index++;
+			}
+		}
+	}
+}
+
